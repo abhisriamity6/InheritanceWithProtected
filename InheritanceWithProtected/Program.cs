23e444117f5a17fbf2df9296488efc0d39b9adba
Administrator <abhi_sri_amity6@yahoo.com>
2017-06-27 23:37:57 +0530

    completed

--- InheritanceWithProtected/Program.cs
+++ InheritanceWithProtected/Program.cs
@@ -6,10 +6,51 @@ using System.Threading.Tasks;
 
 namespace InheritanceWithProtected
 {
-    class Program
+
+    class protectedbaseclass
+    {
+        protected int a, b;
+        public void setvalue(int x , int y)
+        {
+            a = x;
+            b = y;
+
+        }
+
+        public void getvalue()
+        {
+            Console.WriteLine(a + " " + b);
+
+        }
+
+    }
+
+    class derivedclass : protectedbaseclass
+    {
+        int k; // private
+               // D can access B's i and j
+        public void mul()
+        {
+            k = a * b;
+        }
+        public void Showk()
+        {
+            Console.WriteLine(k);
+        }
+
+    }
+        class Program
     {
         static void Main(string[] args)
         {
+            derivedclass derivedclassobj = new derivedclass();
+            derivedclassobj.setvalue(10, 20);
+            derivedclassobj.getvalue();
+            derivedclassobj.mul();
+            derivedclassobj.Showk();
+
+            Console.Read();
+
         }
     }
 }
